0e48d42ff56747e3b47fb56ccd0033e9b1d54e3b
Brian Caffey <briancaffey2010@gmail.com>
2018-05-06 22:15:02 -0400

    baseball-2 added form to run simulations and display results

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+from utils.utils import run_sim
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    if request.method == "POST":
+        print("post made")
+        team1 = request.form.get('team1')
+        team2 = request.form.get('team2')
+        num_games = int(request.form.get('num_games'))
+        data = run_sim(team1, team2, num_games)
+        return render_template('index.html', **data)
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
